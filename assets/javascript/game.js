46f079110479b12e5842cfc76ccbb713d138b3e1
Herculano Ramos <herc@Herculanos-MBP.fios-router.home>
2018-08-13 18:29:17 -0400

    added header

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,8 @@
     var wins = 0;
     var losses = 0;
     var guesses = 10;
-    var usedArray = [];
+    
+    
 
 
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -15,6 +16,10 @@
 
     // When the user presses a key, check if the key is identical if  key is identical  W +1 else L + 1...
 
+   
+    
+    
+    
     document.onkeypress = function (event) {
         var userGuess = event.key;
         console.log(userGuess);
@@ -22,6 +27,8 @@
         if (userGuess === computerChoice) {
             wins++;
             guesses = 10;
+            
+
             
             
             
@@ -35,14 +42,19 @@
         // computerchoices reset after 10 wrong guesses and adds +1 to losses
         if (guesses === 0) {
             losses++;
-            usedArray = [];
             guesses = 10;
+            
         }
+
+        
+
 
         document.getElementById('wins').innerHTML = wins;
         document.getElementById('losses').innerHTML = losses;
         document.getElementById('guesses').innerHTML = guesses;
 
     }
+
+    
 
 
