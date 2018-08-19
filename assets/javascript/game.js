59d8e4800425ebc2d017f934f5b8f4af6ade71f4
Herculano Ramos <herc@Herculanos-MBP.fios-router.home>
2018-08-18 23:52:26 -0400

    letter guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,60 @@
 
-        var alphabet = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    // Variables for tracking our wins, losses and ties. They begin at 0.
-    var wins = 0;
-    var losses = 0;
-    var guesses = 10;
-    
-    
+// Variables for tracking our wins, losses and ties. They begin at 0.
+var wins = 0;
+var losses = 0;
+var guesses = 10;
 
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    console.log(computerChoice)
-
-    // When the user presses a key, check if the key is identical if  key is identical  W +1 else L + 1...
-
-   
-    
-    
-    
-    document.onkeypress = function (event) {
-        var userGuess = event.key;
-        console.log(userGuess);
-        // this is the correct guess 
-        if (userGuess === computerChoice) {
-            wins++;
-            guesses = 10;
-            
-
-            
-            
-            
-            console.log(computerChoice);
-            
-            //wrong choice will minus 1 from guesses
-        } if (userGuess !== computerChoice) {
-            guesses--;
-        }
-
-        // computerchoices reset after 10 wrong guesses and adds +1 to losses
-        if (guesses === 0) {
-            losses++;
-            guesses = 10;
-            
-        }
-
-        
 
 
-        document.getElementById('wins').innerHTML = wins;
-        document.getElementById('losses').innerHTML = losses;
-        document.getElementById('guesses').innerHTML = guesses;
+var computerChoice;
 
+
+
+function compGuess() {
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+}
+
+compGuess();
+
+// When the user presses a key, check if the key is identical if  key is identical  W +1 else L + 1...
+
+
+
+
+
+document.onkeypress = function (event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+    // this is the correct guess 
+    if (userGuess === computerChoice) {
+        wins++;
+        guesses = 10;
+        compGuess();
+        console.log(computerChoice);
+
+        //wrong choice will minus 1 from guesses
+    } else {
+        guesses--;
     }
 
-    
+    // computerchoices reset after 10 wrong guesses and adds +1 to losses
+    if (guesses === 0) {
+        losses++;
+        guesses = 10;
+        compGuess();
+        console.log(computerChoice);
+    }
+
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('guesses').innerHTML = guesses;
+}
+
+
 
 
